05cf61c3382bd6e8e25963b09012412f71071ae1
flinkmao <mxw712@gmail.com>
2024-04-03 09:37:02 +0800

    feature: 获取交易日区间和天数支持所有的可能参数

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -8,27 +8,70 @@ from pyspark.sql.functions import col, min, max, count, lit
 pub_date_table = "edw.t10_pub_date"
 
 
-def get_last_year_begin_month(busi_date):
-    return str(int(busi_date[:4]) - 1) + "01"
+def get_date_period_and_days(
+        spark,
+        begin_date=None,
+        end_date=None,
+        begin_month=None,
+        end_month=None,
+        busi_year=None,
+        busi_month=None,
+        is_trade_day=True
+):
+    # 基于开始和结束日期进行过滤
+    if begin_date and end_date:
+        date_filter = (col("busi_date").between(begin_date, end_date))
+    elif begin_date:
+        date_filter = (col("busi_date") >= begin_date)
+    elif end_date:
+        date_filter = (col("busi_date") <= end_date)
+    else:
+        date_filter = lit(True)
 
+    # 基于开始和结束月份进行过滤
+    if begin_month and end_month:
+        month_range_filter = (
+            (col("busi_date").substr(1, 6) >= begin_month) &
+            (col("busi_date").substr(1, 6) <= end_month)
+        )
+    elif begin_month:
+        month_range_filter = (col("busi_date").substr(1, 6) >= begin_month)
+    elif end_month:
+        month_range_filter = (col("busi_date").substr(1, 6) <= end_month)
+    else:
+        month_range_filter = lit(True)
 
-def get_last_year_end_month(busi_date):
-    return str(int(busi_date[:4]) - 1) + "12"
+    # 基于单独的月份进行过滤
+    if busi_month:
+        month_filter = (col("busi_date").substr(1, 6) == busi_month)
+    else:
+        month_filter = lit(True)
 
+    # 基于单独的年份进行过滤
+    if busi_year:
+        year_filter = (col("busi_date").substr(1, 4) == busi_year)
+    else:
+        year_filter = lit(True)
 
-def get_date_period_and_days_this_month(spark, busi_month, trade_flag="1"):
+    # 基于交易日进行过滤
+    trade_day_filter = ((col("trade_flag") == "1") if is_trade_day else lit(True))
+
+    # Apply all filters
     df_date = spark.table(pub_date_table) \
         .filter(
-        (col("busi_date").substr(1, 6) == busi_month) &
-        (col("market_no") == "1") &
-        ((col("trade_flag") == "1") if trade_flag == "1" else lit(True))
+            date_filter &
+            month_range_filter &
+            month_filter &
+            trade_day_filter &
+            year_filter &
+            (col("market_no") == "1")
     ).agg(
         min("busi_date").alias("v_begin_date"),
         max("busi_date").alias("v_end_date"),
         count("*").alias("v_trade_days")
     )
 
-    if trade_flag == "1":
+    if is_trade_day:
         return (df_date.first()["v_begin_date"],
                 df_date.first()["v_end_date"],
                 df_date.first()["v_trade_days"])
