baedb2c0d462cf498bc33af9d1fbf393ff03fc90
flinkmao <mxw712@gmail.com>
2024-04-19 13:39:13 +0800

    feature: 删除sql提示

--- utils/P_COCKPIT_BUSI_ANA_LINE_M_DATA.py
+++ utils/P_COCKPIT_BUSI_ANA_LINE_M_DATA.py
@@ -311,59 +311,6 @@ def p_cockpit_busi_ana_line_m_data(spark, busi_date):
     市场地位 --日均权益，手续费
     """
 
-    """
-      merge into CF_BUSIMG.T_COCKPIT_BUSI_ANALYSE_LINE_M a
-  using (
-    with tmp as
-     (SELECT T.FUND_ACCOUNT_ID,
-             D.Business_Line_Id,
-             SUM(T.RIGHTS) AS RIGHTS,
-             SUM(NVL(T.TRANSFEE, 0) + NVL(T.DELIVERY_TRANSFEE, 0) +
-                 NVL(T.STRIKEFEE, 0)) AS TRANSFEE
-        FROM CF_SETT.T_CLIENT_SETT T
-        LEFT JOIN CF_SETT.T_FUND_ACCOUNT B
-          ON T.FUND_ACCOUNT_ID = B.FUND_ACCOUNT_ID
-       INNER JOIN CF_BUSIMG.T_CTP_BRANCH_OA_RELA C
-          ON B.BRANCH_ID = C.CTP_BRANCH_ID
-        inner join CF_BUSIMG.T_OA_BRANCH d
-          on c.oa_branch_id = d.departmentid
-       WHERE T.BUSI_DATE BETWEEN v_now_begin_date AND v_now_end_date
-         AND D.Business_Line_Id IS NOT NULL
-       GROUP BY T.FUND_ACCOUNT_ID, D.Business_Line_Id),
-    TMP1 AS
-     (SELECT T.Business_Line_Id,
-             SUM(CASE
-                   WHEN v_now_trade_days <> 0 THEN
-                    T.RIGHTS / v_now_trade_days
-                   ELSE
-                    0
-                 END) AS AVG_RIGHTS,
-             SUM(T.TRANSFEE) AS TRANSFEE
-        FROM TMP T
-       GROUP BY T.Business_Line_Id)
-    SELECT T.Business_Line_Id,
-           (CASE
-             WHEN v_total_rights <> 0 THEN
-              T.AVG_RIGHTS / v_total_rights
-             ELSE
-              0
-           END) AS AVG_RIGHTS_MARKET_RATE,
-           (CASE
-             WHEN v_total_index_value <> 0 THEN
-              T.TRANSFEE / v_total_index_value
-             ELSE
-              0
-           END) AS FUTU_TRANS_INCOME_MARKET_RATE
-      FROM TMP1 T
-
-    ) Y
-        ON (A.Business_Line_Id = Y.Business_Line_Id AND A.BUSI_MONTH = I_MONTH_ID) WHEN
-     MATCHED THEN
-      UPDATE
-         SET A.AVG_RIGHTS_MARKET_RATE        = Y.AVG_RIGHTS_MARKET_RATE,
-             A.FUTU_TRANS_INCOME_MARKET_RATE = Y.FUTU_TRANS_INCOME_MARKET_RATE
-    """
-
     tmp = spark.table("ddw.h15_t_client_sett").alias("t") \
         .filter(
         (col("t.busi_date").between(v_now_begin_date, v_now_end_date))
@@ -441,59 +388,6 @@ def p_cockpit_busi_ana_line_m_data(spark, busi_date):
 
     """
     市场地位 --日均权益，手续费
-    """
-
-    """
-      merge into CF_BUSIMG.T_COCKPIT_BUSI_ANALYSE_LINE_M a
-  using (
-    with tmp as
-     (SELECT T.FUND_ACCOUNT_ID,
-             D.Business_Line_Id,
-             SUM(T.Done_Amt) AS Done_amount,
-             SUM(t.done_sum) AS done_money
-												 
-        FROM CF_SETT.t_Hold_Balance T
-        LEFT JOIN CF_SETT.T_FUND_ACCOUNT B
-          ON T.FUND_ACCOUNT_ID = B.FUND_ACCOUNT_ID
-       INNER JOIN CF_BUSIMG.T_CTP_BRANCH_OA_RELA C
-          ON B.BRANCH_ID = C.CTP_BRANCH_ID
-        inner join CF_BUSIMG.T_OA_BRANCH d
-          on c.oa_branch_id = d.departmentid
-       WHERE T.BUSI_DATE BETWEEN v_now_begin_date AND v_now_end_date
-         AND D.Business_Line_Id IS NOT NULL
-       GROUP BY T.FUND_ACCOUNT_ID, D.Business_Line_Id),
-    TMP1 AS
-     (SELECT T.Business_Line_Id,
-             SUM(t.Done_amount) AS Done_amount,
-             SUM(T.done_money) AS done_money
-											   
-					   
-					 
-									
-										
-        FROM TMP T
-       GROUP BY T.Business_Line_Id)
-    SELECT T.Business_Line_Id,
-           (CASE
-             WHEN v_total_done_amount <> 0 THEN
-              T.Done_amount / v_total_done_amount
-             ELSE
-              0
-           END) AS DONE_AMOUNT_MARKET_RATE,
-           (CASE
-             WHEN v_total_done_money <> 0 THEN
-              T.done_money / v_total_done_money
-             ELSE
-              0
-           END) AS DONE_MONEY_MAREKT_RATE
-      FROM TMP1 T
-
-    ) Y
-        ON (A.Business_Line_Id = Y.Business_Line_Id AND A.BUSI_MONTH = I_MONTH_ID) WHEN
-     MATCHED THEN
-      UPDATE
-         SET A.DONE_MONEY_MAREKT_RATE  = Y.DONE_MONEY_MAREKT_RATE,
-             A.DONE_AMOUNT_MARKET_RATE = Y.DONE_AMOUNT_MARKET_RATE
     """
 
     tmp = spark.table("ddw.h15_hold_balance").alias("t") \
