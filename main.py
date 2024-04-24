b6539a58ed1a73af4c93799ac3656b0cc68bccd3
flinkmao <mxw712@gmail.com>
2024-04-24 17:44:53 +0800

    feature: 进行UDF测试

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from utils.P_COCKPIT_00092_DATA import p_cockpit_00092_data
 from utils.P_COCKPIT_00120_DATA import p_cockpit_00120_data
 from utils.P_COCKPIT_00121_DATA import p_cockpit_00121_data
 from utils.P_COCKPIT_00127_DATA import p_cockpit_00127_data
 from utils.P_COCKPIT_00128_DATA import p_cockpit_00128_data
 from utils.P_COCKPIT_00133_DATA import p_cockpit_00133_data
-from utils.task_env import create_env
+from utils.task_env import *
+from utils.date_utils import *
 from utils.parse_arguments import parse_arguments
 
 logging.basicConfig(level=logging.INFO)
@@ -26,6 +28,10 @@ if __name__ == '__main__':
 
     # p_cockpit_00128_data(spark, busi_date)
 
-    p_cockpit_00133_data(spark, busi_date)
+    # p_cockpit_00092_data(spark, busi_date)
+
+    print("前一个工作日", get_trade_date(spark, busi_date, -1))
+    print("后一个工作日", get_trade_date(spark, busi_date, 1))
+    print("对于非交易日的过去最近的交易日", get_trade_date(spark, busi_date, 0))
 
     logging.info("任务执行完成")
