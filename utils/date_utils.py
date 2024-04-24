d3eefd035fe20f00a79417fb686be71eb5c3e29f
flinkmao <mxw712@gmail.com>
2024-04-24 14:57:51 +0800

    feature: 新增:交易日检查,和寻找前后交易日方法. fix: 去除第三方包

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -4,7 +4,6 @@
 专门处理日期参数的工具类
 """
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 
 from pyspark.sql.functions import col, min, max, count, lit
 
@@ -134,13 +133,91 @@ def get_previous_year_date(busi_date):
     """
     获取给定日期的前一年日期
     :param busi_date: 日期字符串, 格式为 'YYYYMMDD'
-    :return: 前一年日期字符串, 格式为 'YYYYMMDD',闰年2月29日返回2月28日
+    :return: 前一年日期字符串, 格式为 'YYYYMMDD', 闰年2月29日返回2月28日
     """
     # 将输入日期字符串解析为 datetime 对象
     input_date = datetime.strptime(busi_date, '%Y%m%d')
 
+    # 获取输入日期的年份和月份
+    input_year = input_date.year
+    input_month = input_date.month
+    input_day = input_date.day
+
     # 计算前一年的日期
-    previous_year_date = input_date - relativedelta(years=1)
+    previous_year = input_year - 1
+
+    # 如果输入日期是闰年2月29日，而前一年不是闰年，返回2月28日
+    if input_month == 2 and input_day == 29 and not is_leap_year(previous_year):
+        return '{:04d}0228'.format(previous_year)
+
+    # 否则返回前一年的相同日期
+    return '{:04d}{:02d}{:02d}'.format(previous_year, input_month, input_day)
+
+
+def is_leap_year(year):
+    """
+    判断给定年份是否是闰年
+    :param year: 年份
+    :return: True 如果是闰年，否则 False
+    """
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+
+def is_trade_day_check(spark, busi_date):
+    """
+    判断给定日期是否交易日
+    :param spark: SparkSession 对象
+    :param busi_date: 日期字符串, 格式为 'YYYYMMDD'
+    :return: 是否交易日, 布尔值
+    """
+    # 从数据库中查询给定日期的交易标志
+    df_trade_flag = spark.table(pub_date_table) \
+        .filter(
+        (col("busi_date") == busi_date) &
+        (col("trade_flag") == "1") &
+        (col("market_no") == "1")
+    ).select(
+        count("*").alias("v_trade_flag")
+    )
+
+    # 如果查询结果中有交易标志, 则返回 True, 否则返回 False
+    return df_trade_flag.first()["v_trade_flag"] > 0
+
+
+def get_trade_date(spark, busi_date, n):
+    """
+    获取给定日期之后或之前的第 n 个交易日
+    :param spark: SparkSession 对象
+    :param busi_date: 日期字符串, 格式为 'YYYYMMDD'
+    :param n: 整数, 表示第 n 个交易日
+    :return: 第 n 个交易日的日期字符串, 格式为 'YYYYMMDD'
+    """
+    # 将输入日期字符串解析为 datetime 对象
+    input_date = datetime.strptime(busi_date, '%Y%m%d')
+
+    # 初始化input_date为交易日(往前找)
+    while not is_trade_day_check(spark, input_date.strftime('%Y%m%d')):
+        input_date -= timedelta(days=1)
+
+    # 计算第 n 个交易日的日期
+    if n > 0:
+        # 如果 n 大于 0, 则向后查找第 n 个交易日
+        trade_date = input_date
+        while n > 0:
+            # 将日期加一天
+            trade_date += timedelta(days=1)
+            # 如果加一天后是交易日, 则 n 减一
+            if is_trade_day_check(spark, trade_date.strftime('%Y%m%d')):
+                n -= 1
+    else:
+        # 如果 n 小于 0, 则向前查找第 n 个交易日
+        trade_date = input_date
+        while n < 0:
+            # 将日期减一天
+            trade_date -= timedelta(days=1)
+            # 如果减一天后是交易日, 则 n 加一
+            if is_trade_day_check(spark, trade_date.strftime('%Y%m%d')):
+                n += 1
 
     # 将结果格式化为字符串并返回
-    return previous_year_date.strftime('%Y%m%d')
+    return trade_date.strftime('%Y%m%d')
