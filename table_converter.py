a9e62db6cb4e122938877a91247d4f871f9bd403
毛新伟 <mxw712@gmail.com>
2024-04-29 23:40:08 +0800

    fix: 微调

--- table_converter.py
+++ table_converter.py
@@ -10,6 +10,10 @@ from os import path
 print(os.getcwd())
 # 当前目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
+# 项目目录
+project_dir = current_dir
+# table_info.csv 文件路径
+table_info_file = path.join(project_dir, 'input/table_info.csv')
 
 
 # 读取 CSV 文件并转换为字典列表
@@ -27,7 +31,12 @@ def csv_to_dict_list(file_path):
     return data
 
 
-table_info = csv_to_dict_list(path.join(current_dir, 'input/table_info.csv'))
+table_info = csv_to_dict_list(table_info_file)
+table_info_clean = []
 
 for item in table_info:
-    print(item.get('oracle_table_full_name'))
+    oracle_info = item.get('oracle_table_full_name').strip().split('.')
+    oracle_db = oracle_info[0].upper()
+    oracle_table = oracle_info[1].upper()
+    hive_table_full_name = item.get('hive_table_fullname').strip().lower()
+
