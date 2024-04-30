dc1d5636beb83e26c244442ff99d822adc7fd151
flinkmao <mxw712@gmail.com>
2024-04-30 18:01:40 +0800

    feature: 规范化,集中化参数配置

---
+++ config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def load_config(filename):
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config
+
+
+class Config:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = load_config(filename)
+
+    def get(self, key):
+        return self.data.get(key)
