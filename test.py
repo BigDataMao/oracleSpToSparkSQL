9a36fc79ff69d66369456fed40459b3d9e16e3d1
毛新伟 <mxw712@gmail.com>
2024-05-05 22:37:11 +0800

    feature: pyhive连接hive集群测试

--- test.py
+++ test.py
@@ -1,33 +1,16 @@
 # -*- coding: utf-8 -*-
-# 本机运行测试任务: https://blog.csdn.net/qq_39950572/article/details/136260712
-# pip install pyspark==2.4.4
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
-
-from utils.date_utils import *
-
-spark = SparkSession.builder \
-    .appName("test") \
-    .enableHiveSupport() \
-    .getOrCreate()
-
-get_trade_date_udf = udf(get_trade_date, StringType())
-# 创建测试DataFrame
-test_df = spark.createDataFrame([
-    ("20240101",),
-    ("20240102",),
-    ("20240103",),
-    ("20240104",),
-    ("20240105",)
-], ["busi_date"])
-
-# 添加交易日期列
-result_df = test_df.withColumn(
-    "trade_date",
-    get_trade_date_udf(spark, col("busi_date"), lit('0'))
+from pyhive import hive
+conn = hive.Connection(
+    host='cdh-master',
+    port=10000,
+    username='root'
 )
+curser = conn.cursor()
+query = """
+desc ods.t_ds_crm_broker
+"""
+curser.execute(query)
+print(curser.fetchall())
 
-result_df.show()
-
-
+curser.close()
+conn.close()
