ee5ac7aadb15e957565e7122ec42f82319f6354c
flinkmao <mxw712@gmail.com>
2024-05-11 11:58:12 +0800

    fix: 空跑不报错

--- utils/P_COCKPIT_BUSI_ANA_TOP_PD_DATA.py
+++ utils/P_COCKPIT_BUSI_ANA_TOP_PD_DATA.py
@@ -1,70 +1,23 @@
 # -*- coding: utf-8 -*-
-"""
-经营分析-业务单位-成交品种排名落地数据
-"""
-import logging
-from datetime import datetime, timedelta
 
 from pyspark.sql import Window
-from pyspark.sql.functions import col, lit, sum, rank
+from pyspark.sql.functions import sum, rank
 
 from utils.date_utils import *
 from utils.task_env import *
 
+logger = logging.getLogger("logger")
 
+
+@log
 def p_cockpit_busi_ana_top_pd_data(spark, busi_date):
-    logging.info("p_cockpit_busi_ana_top_pd_data开始计算")
-
     """
-  select min(t.busi_date), max(t.busi_date)
-    into v_begin_date, v_end_date
-    from cf_sett.t_pub_date t
-   where substr(t.busi_date,1,6)=I_MONTH_ID
-     and t.market_no = '1'
-     and t.trade_flag = '1';
-
-  delete from CF_BUSIMG.T_COCKPIT_BUSI_ANALYSE_TOP_PD t
-   where t.busi_month = I_MONTH_ID;
-  commit;
-
-
-  INSERT INTO CF_BUSIMG.T_COCKPIT_BUSI_ANALYSE_TOP_PD
-    (BUSI_MONTH,
-     OA_BRANCH_ID,
-     PRODUCT_ID,
-     PRODUCT_NAME,
-     DONE_AMOUNT,
-     DONE_MONEY,
-     RANK_DONE_AMOUNT,
-     RANK_DONE_MONEY)
-    with tmp as
-     (SELECT t.product_id,
-             d.product_name,
-             C.OA_BRANCH_ID,
-             SUM(T.Done_Amt) AS Done_amount,
-             SUM(t.done_sum) AS done_money
-        FROM CF_SETT.t_Hold_Balance T
-        LEFT JOIN CF_SETT.T_FUND_ACCOUNT B
-          ON T.FUND_ACCOUNT_ID = B.FUND_ACCOUNT_ID
-       INNER JOIN CF_BUSIMG.T_CTP_BRANCH_OA_RELA C
-          ON B.BRANCH_ID = C.CTP_BRANCH_ID
-        left join cf_sett.t_product d
-          on t.product_id = d.product_id
-         and t.market_id = d.market_id
-         and t.trade_type = d.trade_type
-       WHERE T.BUSI_DATE BETWEEN v_begin_date AND v_end_date
-         AND C.OA_BRANCH_ID IS NOT NULL
-       GROUP BY t.product_id, d.product_name, C.OA_BRANCH_ID)
-    select I_MONTH_ID as busi_month,
-           t.oa_branch_id,
-           t.product_id,
-           t.product_name,
-           t.Done_amount,
-           t.done_money,
-           row_number() over(partition by t.oa_branch_id order by t.Done_amount desc) as RANK_DONE_AMOUNT,
-           row_number() over(partition by t.oa_branch_id order by t.done_money desc) RANK_DONE_MONEY
-      from tmp t;
+    经营分析-业务单位-成交品种排名落地数据
+    :param spark: SparkSession对象
+    :param busi_date: 业务日期,格式为"YYYYMMDD"
+    :return: None
     """
+    logger.info("p_cockpit_busi_ana_top_pd_data开始计算")
 
     (
         v_begin_date,
@@ -78,7 +31,7 @@ def p_cockpit_busi_ana_top_pd_data(spark, busi_date):
 
     # TODO: CF_BUSIMG.T_COCKPIT_BUSI_ANALYSE_TOP_PD,分区字段,busi_month
 
-    tmp = spark.table("ddw.h15_hold_balance").alias("t") \
+    tmp = spark.table("edw.h15_hold_balance").alias("t") \
         .filter(
         (col("t.busi_date").between(v_begin_date, v_end_date))
     ).join(
@@ -90,7 +43,7 @@ def p_cockpit_busi_ana_top_pd_data(spark, busi_date):
         on=col("b.branch_id") == col("c.ctp_branch_id"),
         how="inner"
     ).join(
-        other=spark.table("ddw.h13_product").alias("d"),
+        other=spark.table("edw.h13_product").alias("d"),
         on=(
             (col("t.product_id") == col("d.product_id")) &
             (col("t.market_id") == col("d.market_id")) &
@@ -132,10 +85,8 @@ def p_cockpit_busi_ana_top_pd_data(spark, busi_date):
         spark=spark,
         df_result=df_result,
         target_table="ddw.T_COCKPIT_BUSI_ANALYSE_TOP_PD",
-        insert_mode="overwrite",
-        partition_column="BUSI_MONTH",
-        partition_value=busi_date[:6]
+        insert_mode="overwrite"
     )
 
-    logging.info("p_cockpit_busi_ana_top_pd_data计算完成")
-    logging.info("本次任务为: 经营分析-业务单位-成交品种排名落地数据")
+    logger.info("p_cockpit_busi_ana_top_pd_data计算完成")
+    logger.info("本次任务为: 经营分析-业务单位-成交品种排名落地数据")
